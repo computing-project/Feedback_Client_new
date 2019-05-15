ddc127815368598ef1acca681a5d9027346cea07
Tanzk2018 <49629750+Tanzk2018@users.noreply.github.com>
2019-05-16 01:52:07 +1000

    cannot solve group student page problem

--- app/src/main/java/com/example/feedback/Activity_Student_Group.java
+++ app/src/main/java/com/example/feedback/Activity_Student_Group.java
@@ -45,6 +45,8 @@ public class Activity_Student_Group extends Activity {
     String surname;
     String email;
 
+    double rawX, rawY;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,6 +68,36 @@ public class Activity_Student_Group extends Activity {
         myAdapter = new MyAdapter(students, this);
 
         listView.setAdapter(myAdapter);
+
+        listView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action)
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        rawX = event.getX();
+                        rawY = event.getY();
+//                        System.out.println("横坐标: "+rawX+"  纵坐标: "+rawY);
+//                        System.out.println("第几个开始:"+listView.pointToPosition((int)rawX, (int)event.getRawY()));
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+//                            double moveX = event.getX() - rawX;
+//                            double moveY = event.getY() - rawY;
+//                            System.out.println("移动过程中坐标: "+moveX+" "+moveY);
+//                            if(moveX > 400)
+//                                v.setBackgroundColor(Color.GRAY);
+                        break;
+                    case MotionEvent.ACTION_UP:
+//                        System.out.println("第几个:"+listView.pointToPosition((int)event.getX(), (int)event.getY()));
+                        break;
+
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     //button delete click.
@@ -96,7 +128,7 @@ public class Activity_Student_Group extends Activity {
 
         @Override
         public Object getItem(int position) {
-            return null;
+            return position;
         }
 
         @Override
@@ -132,108 +164,139 @@ public class Activity_Student_Group extends Activity {
             });
 
 
-            final View dragView = convertView;
-            convertView.setOnLongClickListener(new View.OnLongClickListener() {
+//            final View dragView = convertView;
+////            convertView.setOnLongClickListener(new View.OnLongClickListener() {
+////                @Override
+////                public boolean onLongClick(View v) {
+////                    // DND框架要求传递的数据
+////                    ClipData.Item item = new ClipData.Item(String.valueOf(position));
+////
+////                    ClipData clipData = new ClipData("", new String[] {
+////                            ClipDescription.MIMETYPE_TEXT_PLAIN
+////                    }, item);
+////
+////                    // 开始当前View的拖动操作，将当前拖动对象的position当作localState传递到拖动事件中
+////                    dragView.startDrag(clipData, new View.DragShadowBuilder(dragView), position_use, 0);
+////                    return true;
+////                }
+////            });
+////
+////            convertView.setOnDragListener(new View.OnDragListener() {
+////                @Override
+////
+////
+////                public boolean onDrag(View v, DragEvent event) {
+////
+////                    // Defines a variable to store the action type for the incoming event
+////                    final int action = event.getAction();
+////
+////                    // Handles each of the expected events
+////                    switch(action) {
+////
+////                        case DragEvent.ACTION_DRAG_STARTED:
+////                            // Determines if this View can accept the dragged data
+////                            if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+////                                v.invalidate();
+////
+////                                return true;
+////                            }
+////                            return false;
+////
+////                        case DragEvent.ACTION_DRAG_ENTERED:
+////
+////                            v.setBackgroundColor(Color.GREEN);
+////                            v.invalidate();
+////
+////                            return true;
+////
+////                        case DragEvent.ACTION_DRAG_LOCATION:
+////                            return true;
+////
+////                        case DragEvent.ACTION_DRAG_EXITED:
+////                            v.setBackgroundColor(Color.TRANSPARENT);
+////                            v.invalidate();
+////                            return true;
+////
+////                        case DragEvent.ACTION_DROP:
+////
+////                            v.setBackgroundColor(Color.TRANSPARENT);
+////                            // Gets the item containing the dragged data
+////                            ClipData.Item item = event.getClipData().getItemAt(0);
+////                            final int srcPosition = Integer.parseInt(item.getText().toString());
+////
+////                            //change group num
+////                            if(students.get(position).getGroup()!= -999)
+////                                students.get(srcPosition).setGroup(students.get(position).getGroup());
+////                            else
+////                            {
+////                                int maxGroupNumNow = AllFunctions.getObject().getMaxGroupNumber(indexOfProject);
+////                                students.get(srcPosition).setGroup(maxGroupNumNow+1);
+////                                students.get(position).setGroup(maxGroupNumNow+1);
+////                            }
+////
+////                            StudentInfo studentTemporary = students.get(srcPosition);
+////                            students.remove(srcPosition);
+////                            if(position+1>students.size())
+////                                students.add(studentTemporary);
+////                            else
+////                                students.add(position+1,studentTemporary);
+////
+////                            // myAdapter.notifyDataSetChanged();
+////                            init(indexOfProject);
+////
+////                            // Invalidates the view to force a redraw
+////                            v.invalidate();
+////
+////                            // Returns true. DragEvent.getResult() will return true.
+////                            return true;
+////
+////                        case DragEvent.ACTION_DRAG_ENDED:
+////
+////                            // Turns off any color tinting
+////                            // Invalidates the view to force a redraw
+////                            v.invalidate();
+////
+////                            // returns true; the value is ignored.
+////                            return true;
+////                        // An unknown action type was received.
+////                        default:
+////                            Log.e("DragDrop Example","Unknown action type received by OnDragListener.");
+////                            break;
+////                    }
+////                    return false;
+////                }
+////
+////            });
+
+    final ListView l = (ListView) parent;
+            convertView.setOnTouchListener(new View.OnTouchListener() {
                 @Override
-                public boolean onLongClick(View v) {
-                    // DND框架要求传递的数据
-                    ClipData.Item item = new ClipData.Item(String.valueOf(position));
+                public boolean onTouch(View v, MotionEvent event) {
+                    int action = event.getAction();
+                    switch (action)
+                    {
+                        case MotionEvent.ACTION_DOWN:
+                            int rawX1 = (int) event.getRawX();
+                            int rawY2 = (int) event.getRawY();
+                            System.out.println("横坐标: "+rawX1+"  纵坐标: "+rawY2);
+                            System.out.println("第几个开始:"+l.pointToPosition(rawX1, rawY2));
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+//                            double moveX = event.getX() - rawX;
+//                            double moveY = event.getY() - rawY;
+//                            System.out.println("移动过程中坐标: "+moveX+" "+moveY);
+//                            if(moveX > 400)
+//                                v.setBackgroundColor(Color.GRAY);
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            System.out.println("第几个:"+listView.pointToPosition((int)event.getX(), (int)event.getY()));
+                            break;
 
-                    ClipData clipData = new ClipData("", new String[] {
-                            ClipDescription.MIMETYPE_TEXT_PLAIN
-                    }, item);
-
-                    // 开始当前View的拖动操作，将当前拖动对象的position当作localState传递到拖动事件中
-                    dragView.startDrag(clipData, new View.DragShadowBuilder(dragView), position_use, 0);
-                    return true;
-                }
-            });
-
-            convertView.setOnDragListener(new View.OnDragListener() {
-                @Override
-
-
-                public boolean onDrag(View v, DragEvent event) {
-
-                    // Defines a variable to store the action type for the incoming event
-                    final int action = event.getAction();
-
-                    // Handles each of the expected events
-                    switch(action) {
-
-                        case DragEvent.ACTION_DRAG_STARTED:
-                            // Determines if this View can accept the dragged data
-                            if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
-                                v.invalidate();
-
-                                return true;
-                            }
-                            return false;
-
-                        case DragEvent.ACTION_DRAG_ENTERED:
-
-                            v.setBackgroundColor(Color.GREEN);
-                            v.invalidate();
-
-                            return true;
-
-                        case DragEvent.ACTION_DRAG_LOCATION:
-                            return true;
-
-                        case DragEvent.ACTION_DRAG_EXITED:
-                            v.setBackgroundColor(Color.TRANSPARENT);
-                            v.invalidate();
-                            return true;
-
-                        case DragEvent.ACTION_DROP:
-
-                            v.setBackgroundColor(Color.TRANSPARENT);
-                            // Gets the item containing the dragged data
-                            ClipData.Item item = event.getClipData().getItemAt(0);
-                            final int srcPosition = Integer.parseInt(item.getText().toString());
-
-                            //change group num
-                            if(students.get(position).getGroup()!= -999)
-                                students.get(srcPosition).setGroup(students.get(position).getGroup());
-                            else
-                            {
-                                int maxGroupNumNow = AllFunctions.getObject().getMaxGroupNumber(indexOfProject);
-                                students.get(srcPosition).setGroup(maxGroupNumNow+1);
-                                students.get(position).setGroup(maxGroupNumNow+1);
-                            }
-
-                            StudentInfo studentTemporary = students.get(srcPosition);
-                            students.remove(srcPosition);
-                            if(position+1>students.size())
-                                students.add(studentTemporary);
-                            else
-                                students.add(position+1,studentTemporary);
-
-                            // myAdapter.notifyDataSetChanged();
-                            init(indexOfProject);
-
-                            // Invalidates the view to force a redraw
-                            v.invalidate();
-
-                            // Returns true. DragEvent.getResult() will return true.
-                            return true;
-
-                        case DragEvent.ACTION_DRAG_ENDED:
-
-                            // Turns off any color tinting
-                            // Invalidates the view to force a redraw
-                            v.invalidate();
-
-                            // returns true; the value is ignored.
-                            return true;
-                        // An unknown action type was received.
                         default:
-                            Log.e("DragDrop Example","Unknown action type received by OnDragListener.");
                             break;
                     }
-                    return false;
+                    return true;
                 }
-
             });
 
             return convertView;
--- app/src/main/java/com/example/feedback/CommunicationForClient.java
+++ app/src/main/java/com/example/feedback/CommunicationForClient.java
@@ -264,8 +264,8 @@ public class CommunicationForClient {
 			} else {
 				//失败跳出
 			}
-		} catch (IOException e1) {
-			e1.printStackTrace();
+		} catch (Exception e1) {
+			System.out.println("Exception in addStudent communication");
 		}
 	}
 
