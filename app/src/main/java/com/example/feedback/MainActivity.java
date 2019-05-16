f0220be581d749247c16a264756ba426a230c591
Tanzk2018 <49629750+Tanzk2018@users.noreply.github.com>
2019-05-16 20:15:00 +1000

    test to connect server

--- app/src/main/java/com/example/feedback/MainActivity.java
+++ app/src/main/java/com/example/feedback/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, Assessment_Preparation_Activity.class);
+        Intent intent = new Intent(this, LoginTest_Activity.class);
         startActivity(intent);
         finish();
 
