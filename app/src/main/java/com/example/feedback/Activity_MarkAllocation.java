3ece339dddb52cab87e8eca1de2bf7607685ca86
Tanzk2018 <49629750+Tanzk2018@users.noreply.github.com>
2019-05-17 00:34:37 +1000

    update markAllocation with commentOnly criteria

--- app/src/main/java/com/example/feedback/Activity_MarkAllocation.java
+++ app/src/main/java/com/example/feedback/Activity_MarkAllocation.java
@@ -27,7 +27,8 @@ public class Activity_MarkAllocation extends Activity {
     private GridView gridView;
     //private ListView listView;
     private ProjectInfo project;
-    private ArrayList<Criteria> markedCriteriaList;
+    private ArrayList<Criteria> allCriteriaList;
+    private int markedCriteriaNum;
     private ExpandableListView expandableListView;
 
     @Override
@@ -45,10 +46,11 @@ public class Activity_MarkAllocation extends Activity {
     public void init()
     {
         project = AllFunctions.getObject().getProjectList().get(indexOfProject);
-        markedCriteriaList = project.getCriteria();
-        System.out.println("检查criteriaList是否传入："+markedCriteriaList.size());
+        allCriteriaList = project.getCriteria();
+        markedCriteriaNum = project.getCriteria().size();
+        allCriteriaList.addAll(project.getCommentList());
 
-        MyAdapter myAdapter = new MyAdapter(markedCriteriaList, this);
+        MyAdapter myAdapter = new MyAdapter(allCriteriaList, this);
         gridView = findViewById(R.id.gridView_CriteriaList_markAllocation);
         gridView.setAdapter(myAdapter);
       //  listView = findViewById(R.id.listView_criteriaList_markAllocation);
@@ -119,102 +121,131 @@ public class Activity_MarkAllocation extends Activity {
 
         @Override
         public View getView(final int position, View convertView, ViewGroup parent) {
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.grid_item_markallocation, parent, false);
-
-            TextView textView_criteriaName = convertView.findViewById(R.id.textView_criteriaName_gridItem);
-            textView_criteriaName.setText(markedCriteriaList.get(position).getName());
-            EditText editText_maxMark = convertView.findViewById(R.id.editText_maxMark_gridItem);
-            editText_maxMark.setText(String.valueOf(criteriaList.get(position).getMaximunMark()));
-            String markIncrement = criteriaList.get(position).getMarkIncrement();
-            if(markIncrement != null)
-            switch (markIncrement)
+            if(position<markedCriteriaNum)
             {
+                convertView = LayoutInflater.from(mContext).inflate(R.layout.grid_item_markallocation, parent, false);
+
+                TextView textView_criteriaName = convertView.findViewById(R.id.textView_criteriaName_gridItem);
+                textView_criteriaName.setText(allCriteriaList.get(position).getName());
+                EditText editText_maxMark = convertView.findViewById(R.id.editText_maxMark_gridItem);
+                editText_maxMark.setText(String.valueOf(criteriaList.get(position).getMaximunMark()));
+                String markIncrement = criteriaList.get(position).getMarkIncrement();
+                if (markIncrement != null)
+                    switch (markIncrement) {
 //                case "quarter":
-                case "1/4":
-                    RadioButton radioButton_quarter = convertView.findViewById(R.id.radioButton_quarter_gridItem);
-                    radioButton_quarter.setChecked(true);
-                    break;
+                        case "1/4":
+                            RadioButton radioButton_quarter = convertView.findViewById(R.id.radioButton_quarter_gridItem);
+                            radioButton_quarter.setChecked(true);
+                            break;
 //                case "half":
-                case "1/2":
-                    RadioButton radioButton_half = convertView.findViewById(R.id.radioButton_half_gridItem);
-                    radioButton_half.setChecked(true);
-                    break;
+                        case "1/2":
+                            RadioButton radioButton_half = convertView.findViewById(R.id.radioButton_half_gridItem);
+                            radioButton_half.setChecked(true);
+                            break;
 //                case "full":
-                case "1":
-                    RadioButton radioButton_full = convertView.findViewById(R.id.radioButton_full_gridItem);
-                    radioButton_full.setChecked(true);
-                    break;
-                default:
-                    break;
-            }
-
-
-            RadioGroup radioGroup = convertView.findViewById(R.id.radioGroup_markIncrement_gridItem);
-            radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(RadioGroup rG, int checkID) {
-                    switch(checkID)
-                    {
-                        case R.id.radioButton_quarter_gridItem:
-//                            criteriaList.get(position).setMarkIncrement("quarter");
-                            criteriaList.get(position).setMarkIncrement("1/4");
-                            break;
-                        case R.id.radioButton_half_gridItem:
-//                            criteriaList.get(position).setMarkIncrement("half");
-                            criteriaList.get(position).setMarkIncrement("1/2");
-                            break;
-                        case R.id.radioButton_full_gridItem:
-//                            criteriaList.get(position).setMarkIncrement("full");
-                            criteriaList.get(position).setMarkIncrement("1");
+                        case "1":
+                            RadioButton radioButton_full = convertView.findViewById(R.id.radioButton_full_gridItem);
+                            radioButton_full.setChecked(true);
                             break;
                         default:
                             break;
                     }
-                }
-            });
-
-            Button button_plus = convertView.findViewById(R.id.button_plus_gridItem);
-            button_plus.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int mark = Integer.parseInt(editText_maxMark.getText().toString());
-                    markedCriteriaList.get(position).setMaximunMark(mark+1);
-                    editText_maxMark.setText(String.valueOf(mark+1));
-                }
-            });
-
-            Button button_minus = convertView.findViewById(R.id.button_minus_gridItem);
-            button_minus.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int mark = Integer.parseInt(editText_maxMark.getText().toString());
-                    markedCriteriaList.get(position).setMaximunMark(mark-1);
-                    editText_maxMark.setText(String.valueOf(mark-1));
-                }
-            });
-
-            Button button_commentDetail = convertView.findViewById(R.id.button_commentsDetail_gridItem);
-            button_commentDetail.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                    LayoutInflater layoutInflater = LayoutInflater.from(Activity_MarkAllocation.this);//获得layoutInflater对象
-                    final View view2 = layoutInflater.from(Activity_MarkAllocation.this).inflate(R.layout.dialog_showcommentcriterialayer_markallocation, null);//获得view对象
-                    TextView textView_CriteriaName = view2.findViewById(R.id.textView_criteriaName_showComment);
-                    textView_CriteriaName.setText(markedCriteriaList.get(position).getName());
-                    ArrayList<SubSection> subSectionsList = markedCriteriaList.get(position).getSubsectionList();
-                    expandableListView = view2.findViewById(R.id.expandablelistView_dialogShowCommentCriteriaLayer);
 
 
-                   MyBaseExpandableListAdapter myBaseExpandableListAdapter = new MyBaseExpandableListAdapter(subSectionsList, view2.getContext());
-                    expandableListView.setAdapter(myBaseExpandableListAdapter);
-                    Dialog dialog = new android.app.AlertDialog.Builder(Activity_MarkAllocation.this).setView(view2).create();
+                RadioGroup radioGroup = convertView.findViewById(R.id.radioGroup_markIncrement_gridItem);
+                radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup rG, int checkID) {
+                        switch (checkID) {
+                            case R.id.radioButton_quarter_gridItem:
+//                            criteriaList.get(position).setMarkIncrement("quarter");
+                                criteriaList.get(position).setMarkIncrement("1/4");
+                                break;
+                            case R.id.radioButton_half_gridItem:
+//                            criteriaList.get(position).setMarkIncrement("half");
+                                criteriaList.get(position).setMarkIncrement("1/2");
+                                break;
+                            case R.id.radioButton_full_gridItem:
+//                            criteriaList.get(position).setMarkIncrement("full");
+                                criteriaList.get(position).setMarkIncrement("1");
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                });
 
-                    dialog.show();
-                }
-            });
+                Button button_plus = convertView.findViewById(R.id.button_plus_gridItem);
+                button_plus.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int mark = Integer.parseInt(editText_maxMark.getText().toString());
+                        allCriteriaList.get(position).setMaximunMark(mark + 1);
+                        editText_maxMark.setText(String.valueOf(mark + 1));
+                    }
+                });
 
-            return convertView;
+                Button button_minus = convertView.findViewById(R.id.button_minus_gridItem);
+                button_minus.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int mark = Integer.parseInt(editText_maxMark.getText().toString());
+                        allCriteriaList.get(position).setMaximunMark(mark - 1);
+                        editText_maxMark.setText(String.valueOf(mark - 1));
+                    }
+                });
+
+                Button button_commentDetail = convertView.findViewById(R.id.button_commentsDetail_gridItem);
+                button_commentDetail.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        LayoutInflater layoutInflater = LayoutInflater.from(Activity_MarkAllocation.this);//获得layoutInflater对象
+                        final View view2 = layoutInflater.from(Activity_MarkAllocation.this).inflate(R.layout.dialog_showcommentcriterialayer_markallocation, null);//获得view对象
+                        TextView textView_CriteriaName = view2.findViewById(R.id.textView_criteriaName_showComment);
+                        textView_CriteriaName.setText(allCriteriaList.get(position).getName());
+                        ArrayList<SubSection> subSectionsList = allCriteriaList.get(position).getSubsectionList();
+                        expandableListView = view2.findViewById(R.id.expandablelistView_dialogShowCommentCriteriaLayer);
+
+
+                        MyBaseExpandableListAdapter myBaseExpandableListAdapter = new MyBaseExpandableListAdapter(subSectionsList, view2.getContext());
+                        expandableListView.setAdapter(myBaseExpandableListAdapter);
+                        Dialog dialog = new android.app.AlertDialog.Builder(Activity_MarkAllocation.this).setView(view2).create();
+
+                        dialog.show();
+                    }
+                });
+
+                return convertView;
+            }
+            else
+            {
+                convertView = LayoutInflater.from(mContext).inflate(R.layout.grid_item_commentonly, parent, false);
+
+                TextView textView_criteriaName = convertView.findViewById(R.id.textView_criteriaName_gridItemCommentOnly);
+                textView_criteriaName.setText(allCriteriaList.get(position).getName());
+                Button button_commentDetail = convertView.findViewById(R.id.button_showComments_gridItemCommentOnly);
+                button_commentDetail.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        LayoutInflater layoutInflater = LayoutInflater.from(Activity_MarkAllocation.this);//获得layoutInflater对象
+                        final View view2 = layoutInflater.from(Activity_MarkAllocation.this).inflate(R.layout.dialog_showcommentcriterialayer_markallocation, null);//获得view对象
+                        TextView textView_CriteriaName = view2.findViewById(R.id.textView_criteriaName_showComment);
+                        textView_CriteriaName.setText(allCriteriaList.get(position).getName());
+                        ArrayList<SubSection> subSectionsList = allCriteriaList.get(position).getSubsectionList();
+                        expandableListView = view2.findViewById(R.id.expandablelistView_dialogShowCommentCriteriaLayer);
+
+
+                        MyBaseExpandableListAdapter myBaseExpandableListAdapter = new MyBaseExpandableListAdapter(subSectionsList, view2.getContext());
+                        expandableListView.setAdapter(myBaseExpandableListAdapter);
+                        Dialog dialog = new android.app.AlertDialog.Builder(Activity_MarkAllocation.this).setView(view2).create();
+
+                        dialog.show();
+                    }
+                });
+                return convertView;
+            }
         }
     }
 
