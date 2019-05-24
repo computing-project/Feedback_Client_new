24fa93e4519596c3e216013fafa3d3d71b9e5b6c
Tanzk2018 <49629750+Tanzk2018@users.noreply.github.com>
2019-05-24 21:32:18 +1000

    update

--- app/src/main/java/com/example/feedback/MainActivity.java
+++ app/src/main/java/com/example/feedback/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, LoginTest_Activity.class);
+        Intent intent = new Intent(this, Activity_Reaper_Mark.class);
         startActivity(intent);
         finish();
 
