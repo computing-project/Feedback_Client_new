3e70d3095259dbbc17bacdd916f9843dec69d701
KarlieHe <43411354+KarlieHe@users.noreply.github.com>
2019-05-29 00:53:47 +1000

    Merge branch 'master' of https://github.com/computing-project/Feedback_Client_new

--- app/src/main/java/com/example/feedback/Activity_showComment_markAllocation.java
+++ app/src/main/java/com/example/feedback/Activity_showComment_markAllocation.java
@@ -12,11 +12,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,6 +32,7 @@ public class Activity_showComment_markAllocation extends Activity {
     private int indexOfShortText = -999;
     private int indexOfProject;
     private int indexOfCriteria;
+    private Spinner spinner_shortText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +48,12 @@ public class Activity_showComment_markAllocation extends Activity {
             criteria = project.getCommentList().get(indexOfCriteria-project.getCriteria().size());
         else
             criteria = project.getCriteria().get(indexOfCriteria);
+        init();
+
+    }
+
+    private void init()
+    {
         ExpandableListView expandableListView_comments_left = findViewById(R.id.expandableListView_showComment);
         MyAdapterForCommentLeft myAdapterForCommentLeft = new MyAdapterForCommentLeft(this,criteria.getSubsectionList());
         expandableListView_comments_left.setAdapter(myAdapterForCommentLeft);
@@ -58,7 +67,6 @@ public class Activity_showComment_markAllocation extends Activity {
                 }
             }
         });
-
     }
 
     public void edit_longText_showComment(View view)
@@ -104,10 +112,94 @@ public class Activity_showComment_markAllocation extends Activity {
         }
     }
 
-    public void add_showComment(View view)
-    {
+        public void add_showComment(View view)
+        {
+            LayoutInflater layoutInflater = LayoutInflater.from(Activity_showComment_markAllocation.this);//获得layoutInflater对象
+            final View view2 = layoutInflater.from(Activity_showComment_markAllocation.this).inflate(R.layout.dialog_add_comment, null);//获得view对象
 
-    }
+            final Spinner spinner_subsection = view2.findViewById(R.id.spinner_subsection_addComment);
+            MyAdapterForSubsectionSpinner myAdapterForSubsectionSpinner = new MyAdapterForSubsectionSpinner(view2.getContext(),criteria.getSubsectionList());
+            spinner_subsection.setAdapter(myAdapterForSubsectionSpinner);
+            spinner_subsection.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    if(position>=criteria.getSubsectionList().size())
+                    {   spinner_shortText = view2.findViewById(R.id.spinner_shortText_addComment);
+                        //MyAdapterForShortTextSpinner myAdapterForShortTextSpinner = new MyAdapterForShortTextSpinner(view2.getContext(), null);
+                        spinner_shortText.setAdapter(null);}
+                    else {
+                        spinner_shortText = view2.findViewById(R.id.spinner_shortText_addComment);
+                        MyAdapterForShortTextSpinner myAdapterForShortTextSpinner = new MyAdapterForShortTextSpinner(view2.getContext(), criteria.getSubsectionList().get(position).getShortTextList());
+                        spinner_shortText.setAdapter(myAdapterForShortTextSpinner);
+                    }
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    return;
+                }
+            });
+
+            Dialog dialog = new AlertDialog.Builder(Activity_showComment_markAllocation.this).setTitle("Add Comment").setView(view2).setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    int whichSubsection = spinner_subsection.getSelectedItemPosition();
+                    if(whichSubsection<criteria.getSubsectionList().size())
+                    {
+                        int whichShortText = spinner_shortText.getSelectedItemPosition();
+                        if(whichShortText < criteria.getSubsectionList().get(whichSubsection).getShortTextList().size())
+                        {
+                            EditText editText_longText = view2.findViewById(R.id.editText_longText_addComment);
+                            String longText = editText_longText.getText().toString();
+                            criteria.getSubsectionList().get(whichSubsection).getShortTextList().get(whichShortText).getLongtext().add(longText);
+                            init();
+                        }
+                        else
+                        {
+                            EditText editText_shortText = view2.findViewById(R.id.editText_shortText_addComment);
+                            String shortText = editText_shortText.getText().toString();
+                            EditText editText_shortTextGrade = view2.findViewById(R.id.editText_shortTextgrade_addComment);
+                            String shortTextGrade = editText_shortTextGrade.getText().toString();
+                            EditText editText_longText = view2.findViewById(R.id.editText_longText_addComment);
+                            String longText = editText_longText.getText().toString();
+                            ShortText shortText_ls = new ShortText();
+                            shortText_ls.setName(shortText);
+                            shortText_ls.setGrade(Integer.parseInt(shortTextGrade));
+                            shortText_ls.getLongtext().add(longText);
+                            criteria.getSubsectionList().get(whichSubsection).getShortTextList().add(shortText_ls);
+                            init();
+                        }
+                    }
+                    else
+                    {
+                        EditText editText_otherSubsection = view2.findViewById(R.id.editText_otherSubsection_addComment);
+                        String subsectionName = editText_otherSubsection.getText().toString();
+                        EditText editText_shortText = view2.findViewById(R.id.editText_shortText_addComment);
+                        String shortText = editText_shortText.getText().toString();
+                        EditText editText_shortTextGrade = view2.findViewById(R.id.editText_shortTextgrade_addComment);
+                        String shortTextGrade = editText_shortTextGrade.getText().toString();
+                        EditText editText_longText = view2.findViewById(R.id.editText_longText_addComment);
+                        String longText = editText_longText.getText().toString();
+                        ShortText shortText_ls = new ShortText();
+                        shortText_ls.setName(shortText);
+                        shortText_ls.setGrade(Integer.parseInt(shortTextGrade));
+                        shortText_ls.getLongtext().add(longText);
+                        SubSection subSection_ls = new SubSection();
+                        subSection_ls.setName(subsectionName);
+                        subSection_ls.getShortTextList().add(shortText_ls);
+                        criteria.getSubsectionList().add(subSection_ls);
+                        init();
+                    }
+                }
+            }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO Auto-generated method stub
+
+                }
+            }).create();
+            dialog.show();
+        }
 
     class MyAdapterForCommentLeft extends BaseExpandableListAdapter
     {
@@ -235,6 +327,93 @@ public class Activity_showComment_markAllocation extends Activity {
                 convertView.setBackgroundColor(Color.GRAY);
             else
                 convertView.setBackgroundColor(Color.TRANSPARENT);
+            return convertView;
+        }
+    }
+
+    class MyAdapterForSubsectionSpinner extends BaseAdapter
+    {
+        private Context mContext;
+        private ArrayList<SubSection> subSections;
+
+        public MyAdapterForSubsectionSpinner(Context context, ArrayList<SubSection> subSections)
+        {
+            this.mContext = context;
+            this.subSections = subSections;
+        }
+
+        @Override
+        public int getCount() {
+            return subSections.size()+1;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            if(convertView == null)
+                convertView = LayoutInflater.from(mContext).inflate(R.layout.list_item_spinner, null);
+            if(position<subSections.size()) {
+                TextView textView = convertView.findViewById(R.id.textView_content_spinner);
+                textView.setText(subSections.get(position).getName());
+            }
+            else
+            {
+                TextView textView = convertView.findViewById(R.id.textView_content_spinner);
+                textView.setText("Others");
+            }
+            return convertView;
+        }
+    }
+
+
+    class MyAdapterForShortTextSpinner extends BaseAdapter
+    {
+        private Context mContext;
+        private ArrayList<ShortText> shortTexts;
+
+        public MyAdapterForShortTextSpinner(Context context, ArrayList<ShortText> shortTexts)
+        {
+            this.mContext = context;
+            this.shortTexts = shortTexts;
+        }
+
+        @Override
+        public int getCount() {
+            return shortTexts.size()+1;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            if(convertView == null)
+                convertView = LayoutInflater.from(mContext).inflate(R.layout.list_item_spinner, null);
+            if(position<shortTexts.size()) {
+                TextView textView = convertView.findViewById(R.id.textView_content_spinner);
+                textView.setText(shortTexts.get(position).getName());
+            }
+            else
+            {
+                TextView textView = convertView.findViewById(R.id.textView_content_spinner);
+                textView.setText("Others");
+            }
             return convertView;
         }
     }
