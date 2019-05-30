4655279b4039c4340668abaa13fb25105c5059c4
Tanzk2018 <49629750+Tanzk2018@users.noreply.github.com>
2019-05-30 19:43:21 +1000

    update

--- app/src/main/java/com/example/feedback/Activity_Editable_group_report.java
+++ app/src/main/java/com/example/feedback/Activity_Editable_group_report.java
@@ -2,26 +2,37 @@ package com.example.feedback;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Activity_editable_individual_report extends Activity {
+import java.util.ArrayList;
+
+public class Activity_Editable_group_report extends Activity {
     private int indexOfProject;
-    private int indexOfStudent;
+    private int indexOfGroup;
     private int indexOfMark;
+    private ArrayList<StudentInfo> studentInfoArrayList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editable_individual_report);
+        setContentView(R.layout.activity__editable_group_report);
+
         Intent intent = getIntent();
         indexOfProject = Integer.parseInt(intent.getStringExtra("indexOfProject"));
-        indexOfStudent = Integer.parseInt(intent.getStringExtra("indexOfStudent"));
+        ProjectInfo project = AllFunctions.getObject().getProjectList().get(indexOfProject);
+        indexOfGroup = Integer.parseInt(intent.getStringExtra("indexOfGroup"));
         indexOfMark = Integer.parseInt(intent.getStringExtra("indexOfMark"));
+        studentInfoArrayList = new ArrayList<StudentInfo>();
+        for(int i=0; i<project.getStudentInfo().size(); i++)
+        {
+            if(project.getStudentInfo().get(i).getGroup() == indexOfGroup)
+                studentInfoArrayList.add(project.getStudentInfo().get(i));
+        }
 
         Button button_back_title = findViewById(R.id.button_back_title);
         button_back_title.setOnClickListener(new View.OnClickListener() {
@@ -36,12 +47,12 @@ public class Activity_editable_individual_report extends Activity {
         textView_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Activity_editable_individual_report.this, Activity_Login.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                Intent intent = new Intent(Activity_Editable_group_report.this, Activity_Login.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
                 finish();
             }
         });
-        Button button_finalReport = findViewById(R.id.button_finalReport_report);
+        Button button_finalReport = findViewById(R.id.button_finalReport_groupReport);
         if(!AllFunctions.getObject().getProjectList().get(indexOfProject).getAssistant().get(0).equals
                 (AllFunctions.getObject().getMyEmail()))
         {
@@ -49,14 +60,16 @@ public class Activity_editable_individual_report extends Activity {
         }
 
         init();
+
+
     }
+
 
     private void init()
     {
         ProjectInfo project = AllFunctions.getObject().getProjectList().get(indexOfProject);
-        StudentInfo student = AllFunctions.getObject().getProjectList().get(indexOfProject).getStudentInfo().get(indexOfStudent);
         Mark mark = AllFunctions.getObject().getMarkListForMarkPage().get(indexOfMark);
-        Button button_edit = findViewById(R.id.button_edit_report);
+        Button button_edit = findViewById(R.id.button_edit_groupReport);
         if(!mark.getLecturerEmail().equals(AllFunctions.getObject().getMyEmail()))
         {
             button_edit.setVisibility(View.INVISIBLE);
@@ -67,19 +80,24 @@ public class Activity_editable_individual_report extends Activity {
         textView_assessorName.setText("Assessor: "+ mark.getLecturerName());
         String htmlString =
                 "<html>" +
-                "<body>" +
-                "<h1 style=\"font-weight: normal\">"+project.getProjectName()+"</h1>" +
-                "<hr>" +
-                "<p>"+ student.getFirstName()+" "+student.getMiddleName()+" "+student.getSurname()+" --- "+student.getNumber() +"</p >" +
-                "<h2 style=\"font-weight: normal\">Subject</h2>" +
-                "<p>"+project.getSubjectCode()+" --- "+project.getSubjectName()+"</p >" +
-                "<h2 style=\"font-weight: normal\">Project</h2>" +
-                "<p>"+project.getProjectName()+"</p >" +
-                "<h2 style=\"font-weight: normal\">Mark Attained</h2>" +
-                "<p>"+mark.getTotalMark()+"%</p >" +
-                "<h2 style=\"font-weight: normal\">Assessor</h2>" + "<p>";
+                        "<body>" +
+                        "<h1 style=\"font-weight: normal\">"+project.getProjectName()+"</h1>" +
+                        "<hr>" +
+                        "<p>"+ "Group: "+indexOfGroup +"</p >" +
+                        "<h2 style=\"font-weight: normal\">Subject</h2>" +
+                        "<p>"+project.getSubjectCode()+" --- "+project.getSubjectName()+"</p >" +
+                        "<h2 style=\"font-weight: normal\">Project</h2>" +
+                        "<p>"+project.getProjectName()+"</p >" +
+                        "<h2 style=\"font-weight: normal\">Mark Attained</h2>" +
+                        "<p>"+mark.getTotalMark()+"%</p >" +
+                        "<h2 style=\"font-weight: normal\">Assessor</h2>" + "<p>";
         for(int i=0; i<project.getAssistant().size(); i++)
             htmlString = htmlString + project.getAssistant().get(i)+"<br>";
+
+        htmlString += "<h2 style=\"font-weight: normal\">Students</h2>" + "<p>";
+        for(int i=0; i<studentInfoArrayList.size(); i++)
+            htmlString = htmlString + studentInfoArrayList.get(i).getNumber()+studentInfoArrayList.get(i).getFirstName()+" "+studentInfoArrayList.get(i).getMiddleName()+" "+studentInfoArrayList.get(i).getSurname()+"<br>";
+
         htmlString = htmlString +
                 "</p >" +
                 "<h2 style=\"font-weight: normal\">Assessment Date</h2>" +
@@ -98,18 +116,18 @@ public class Activity_editable_individual_report extends Activity {
         }
         htmlString +=
                 "</div>" +
-                "</body>" +
-                "</html>";
-        TextView textView_pdfContent = findViewById(R.id.textView_pdfContent_report);
+                        "</body>" +
+                        "</html>";
+        TextView textView_pdfContent = findViewById(R.id.textView_pdfContent_GroupReport);
         textView_pdfContent.setText(Html.fromHtml(htmlString));
     }
 
     public void finalReport(View view)
     {
-        Intent intent = new Intent(Activity_editable_individual_report.this, Activity_SendReport_Individual.class);
+        Intent intent = new Intent(Activity_Editable_group_report.this, Activity_SendReport_Group.class);
         intent.putExtra("indexOfProject",String.valueOf(indexOfProject));
-        intent.putExtra("indexOfStudent",String.valueOf(indexOfStudent));
-        intent.putExtra("indexOfMark",String.valueOf(indexOfMark));
+        intent.putExtra("indexOfGroup",String.valueOf(indexOfGroup));
         startActivity(intent);
     }
 }
+
---
+++ app/src/main/java/com/example/feedback/Activity_SendReport_Group.java
@@ -0,0 +1,128 @@
+package com.example.feedback;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Activity_SendReport_Group extends AppCompatActivity {
+    private int indexOfProject;
+    private int indexOfGroup;
+    private int indexOfMark;
+    private ArrayList<StudentInfo> studentInfoArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__send_report__group);
+
+        Intent intent = getIntent();
+        indexOfProject = Integer.parseInt(intent.getStringExtra("indexOfProject"));
+        ProjectInfo project = AllFunctions.getObject().getProjectList().get(indexOfProject);
+        indexOfGroup = Integer.parseInt(intent.getStringExtra("indexOfGroup"));
+        indexOfMark = Integer.parseInt(intent.getStringExtra("indexOfMark"));
+        studentInfoArrayList = new ArrayList<StudentInfo>();
+        for(int i=0; i<project.getStudentInfo().size(); i++)
+        {
+            if(project.getStudentInfo().get(i).getGroup() == indexOfGroup)
+                studentInfoArrayList.add(project.getStudentInfo().get(i));
+        }
+
+        Button button_back_title = findViewById(R.id.button_back_title);
+        button_back_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        TextView textView_helloUser = findViewById(R.id.textView_helloUser);
+        textView_helloUser.setText("Hello, "+AllFunctions.getObject().getUsername());
+        TextView textView_logout = findViewById(R.id.textView_logout);
+        textView_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity_SendReport_Group.this, Activity_Login.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        init();
+
+    }
+
+    private void init()
+    {
+        ProjectInfo project = AllFunctions.getObject().getProjectList().get(indexOfProject);
+        Mark mark = AllFunctions.getObject().getMarkListForMarkPage().get(indexOfMark);        ArrayList<Mark> markList = AllFunctions.getObject().getMarkListForMarkPage();
+        Button button_sendSingle = findViewById(R.id.button_sendStudent_sendReportGroup);
+        button_sendSingle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(int i=0; i<studentInfoArrayList.size(); i++)
+                    AllFunctions.getObject().sendPDF(project,studentInfoArrayList.get(i).getNumber(),1);
+            }
+        });
+        Button button_sendBoth = findViewById(R.id.button_sendBoth_sendReportGroup);
+        button_sendBoth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(int i=0; i<studentInfoArrayList.size(); i++)
+                    AllFunctions.getObject().sendPDF(project,studentInfoArrayList.get(i).getNumber(),2);
+            }
+        });
+
+        TextView textView_totalMark = findViewById(R.id.textView_totalMark_sendReportGroup);
+        textView_totalMark.setText("Mark:"+(int)markList.get(0).getTotalMark()+"%");
+
+        String htmlString =
+                "<html>" +
+                        "<body>" +
+                        "<h1 style=\"font-weight: normal\">"+project.getProjectName()+"</h1>" +
+                        "<hr>" +
+                        "<p>"+ "Group: "+indexOfGroup +"</p >" +
+                        "<h2 style=\"font-weight: normal\">Subject</h2>" +
+                        "<p>"+project.getSubjectCode()+" --- "+project.getSubjectName()+"</p >" +
+                        "<h2 style=\"font-weight: normal\">Project</h2>" +
+                        "<p>"+project.getProjectName()+"</p >" +
+                        "<h2 style=\"font-weight: normal\">Mark Attained</h2>" +
+                        "<p>"+mark.getTotalMark()+"%</p >" +
+                        "<h2 style=\"font-weight: normal\">Assessor</h2>" + "<p>";
+        for(int i=0; i<project.getAssistant().size(); i++)
+            htmlString = htmlString + project.getAssistant().get(i)+"<br>";
+
+        htmlString += "<h2 style=\"font-weight: normal\">Students</h2>" + "<p>";
+        for(int i=0; i<studentInfoArrayList.size(); i++)
+            htmlString = htmlString + studentInfoArrayList.get(i).getNumber()+studentInfoArrayList.get(i).getFirstName()+" "+studentInfoArrayList.get(i).getMiddleName()+" "+studentInfoArrayList.get(i).getSurname()+"<br>";
+
+        htmlString = htmlString +
+                "</p >" +
+                "<h2 style=\"font-weight: normal\">Assessment Date</h2>" +
+                "<p>"+"test date"+"</p ><br><br><br><hr>" +
+                "<div>";
+        for(int i=0; i<mark.getCriteriaList().size(); i++)
+        {
+            htmlString += "<h3 style=\"font-weight: normal\"><span style=\"float:left\">" + mark.getCriteriaList().get(i).getName() + "</span>" +
+                    "<span style=\"float:right\">"+ mark.getMarkList().get(i) +"/"+ mark.getCriteriaList().get(i).getMaximunMark() + "</span></h3>";
+            for(int j=0; j<mark.getCriteriaList().get(i).getSubsectionList().size(); j++)
+            {
+                htmlString+= "<p>&lt;"+mark.getCriteriaList().get(i).getSubsectionList().get(j).getName()+
+                        ":&gt;"+mark.getCriteriaList().get(i).getSubsectionList().get(j).getShortTextList().get(0).getLongtext()+"</p >";
+            }
+            htmlString += "<br>";
+        }
+        htmlString +=
+                "</div>" +
+                        "</body>" +
+                        "</html>";
+        TextView textView_pdfContent = findViewById(R.id.textView_pdfContent_sendReportGroup);
+        textView_pdfContent.setText(Html.fromHtml(htmlString));
+    }
+}
+
