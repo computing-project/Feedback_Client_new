96893a76579aa9f5c989f9890c63b8c7346f1cb1
Tanzk2018 <49629750+Tanzk2018@users.noreply.github.com>
2019-05-30 20:54:17 +1000

    update N lectures to N students review

--- app/src/main/java/com/example/feedback/Activity_SendReport_Group.java
+++ app/src/main/java/com/example/feedback/Activity_SendReport_Group.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 public class Activity_SendReport_Group extends AppCompatActivity {
     private int indexOfProject;
     private int indexOfGroup;
-    private int indexOfMark;
+//    private int indexOfMark;
     private ArrayList<StudentInfo> studentInfoArrayList;
 
     @Override
@@ -26,7 +26,7 @@ public class Activity_SendReport_Group extends AppCompatActivity {
         indexOfProject = Integer.parseInt(intent.getStringExtra("indexOfProject"));
         ProjectInfo project = AllFunctions.getObject().getProjectList().get(indexOfProject);
         indexOfGroup = Integer.parseInt(intent.getStringExtra("indexOfGroup"));
-        indexOfMark = Integer.parseInt(intent.getStringExtra("indexOfMark"));
+//        indexOfMark = Integer.parseInt(intent.getStringExtra("indexOfMark"));
         studentInfoArrayList = new ArrayList<StudentInfo>();
         for(int i=0; i<project.getStudentInfo().size(); i++)
         {
@@ -60,7 +60,7 @@ public class Activity_SendReport_Group extends AppCompatActivity {
     private void init()
     {
         ProjectInfo project = AllFunctions.getObject().getProjectList().get(indexOfProject);
-        Mark mark = AllFunctions.getObject().getMarkListForMarkPage().get(indexOfMark);        ArrayList<Mark> markList = AllFunctions.getObject().getMarkListForMarkPage();
+        ArrayList<Mark> markList = AllFunctions.getObject().getMarkListForMarkPage();
         Button button_sendSingle = findViewById(R.id.button_sendStudent_sendReportGroup);
         button_sendSingle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,34 +86,39 @@ public class Activity_SendReport_Group extends AppCompatActivity {
                         "<body>" +
                         "<h1 style=\"font-weight: normal\">"+project.getProjectName()+"</h1>" +
                         "<hr>" +
-                        "<p>"+ "Group: "+indexOfGroup +"</p >" +
-                        "<h2 style=\"font-weight: normal\">Subject</h2>" +
+                        "<p>"+ "Group: "+indexOfGroup +"</p >" +                        "<h2 style=\"font-weight: normal\">Subject</h2>" +
                         "<p>"+project.getSubjectCode()+" --- "+project.getSubjectName()+"</p >" +
                         "<h2 style=\"font-weight: normal\">Project</h2>" +
                         "<p>"+project.getProjectName()+"</p >" +
                         "<h2 style=\"font-weight: normal\">Mark Attained</h2>" +
-                        "<p>"+mark.getTotalMark()+"%</p >" +
+                        "<p>"+markList.get(0).getTotalMark()+"%</p >" +
                         "<h2 style=\"font-weight: normal\">Assessor</h2>" + "<p>";
-        for(int i=0; i<project.getAssistant().size(); i++)
+        for(int i=0; i<project.getAssistant().size(); i++) {
             htmlString = htmlString + project.getAssistant().get(i)+"<br>";
-
+        }
         htmlString += "<h2 style=\"font-weight: normal\">Students</h2>" + "<p>";
         for(int i=0; i<studentInfoArrayList.size(); i++)
-            htmlString = htmlString + studentInfoArrayList.get(i).getNumber()+studentInfoArrayList.get(i).getFirstName()+" "+studentInfoArrayList.get(i).getMiddleName()+" "+studentInfoArrayList.get(i).getSurname()+"<br>";
+            htmlString = htmlString + studentInfoArrayList.get(i).getNumber()+"---"+studentInfoArrayList.get(i).getFirstName()+" "+studentInfoArrayList.get(i).getMiddleName()+" "+studentInfoArrayList.get(i).getSurname()+"<br>";
+
 
         htmlString = htmlString +
                 "</p >" +
                 "<h2 style=\"font-weight: normal\">Assessment Date</h2>" +
                 "<p>"+"test date"+"</p ><br><br><br><hr>" +
                 "<div>";
-        for(int i=0; i<mark.getCriteriaList().size(); i++)
+        for(int i=0; i<markList.get(0).getCriteriaList().size(); i++)
         {
-            htmlString += "<h3 style=\"font-weight: normal\"><span style=\"float:left\">" + mark.getCriteriaList().get(i).getName() + "</span>" +
-                    "<span style=\"float:right\">"+ mark.getMarkList().get(i) +"/"+ mark.getCriteriaList().get(i).getMaximunMark() + "</span></h3>";
-            for(int j=0; j<mark.getCriteriaList().get(i).getSubsectionList().size(); j++)
+            htmlString += "<h3 style=\"font-weight: normal\"><span style=\"float:left\">" + markList.get(0).getCriteriaList().get(i).getName() + "</span>" +
+                    "<span style=\"float:right\">   "+ markList.get(0).getMarkList().get(i) +"/"+ markList.get(0).getCriteriaList().get(i).getMaximunMark() + "</span></h3>";
+            for(int j=0; j<markList.size(); j++)
             {
-                htmlString+= "<p>&lt;"+mark.getCriteriaList().get(i).getSubsectionList().get(j).getName()+
-                        ":&gt;"+mark.getCriteriaList().get(i).getSubsectionList().get(j).getShortTextList().get(0).getLongtext()+"</p >";
+                htmlString += "<h4 style=\"font-weight: normal;color: #014085\">"+markList.get(j).getLecturerName()+":</h4>";
+                if(markList.get(j).getCriteriaList().size() > 0)
+                    for(int k=0; k<markList.get(j).getCriteriaList().get(i).getSubsectionList().size(); k++)
+                    {
+                        htmlString += "<p>&lt;" + markList.get(j).getCriteriaList().get(i).getSubsectionList().get(k).getName() + ":&gt;"
+                                + markList.get(j).getCriteriaList().get(i).getSubsectionList().get(k).getShortTextList().get(0).getLongtext() + "</p >";
+                    }
             }
             htmlString += "<br>";
         }
